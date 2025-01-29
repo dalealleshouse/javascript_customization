089bed76754de31adebb442d369333e208455e27
Dale Alleshouse <dalealleshouse@gmail.com>
2025-01-29 18:02:35 -0500

    WIP

--- event_loop_demo/vibe_loop.js
+++ event_loop_demo/vibe_loop.js
@@ -1,12 +1,18 @@
 console.log("Scheduling 4 timeouts...");
+
 setTimeout(1000, () => console.log("Fired after 1s"));
 setTimeout(2000, () => console.log("Fired after 2s"));
 setTimeout(5000, () => console.log("Fired after 5s"));
-setTimeout(8000, timeOutInTimeout);
+setTimeout(8000, () => {
+  console.log("Fired after 8s");
+  console.log("Scheduling yet another timeout...");
+  setTimeout(2000, () => console.log("Fired after another 2s"));
+});
+
 console.log("All timers scheduled!");
 
-function timeOutInTimeout() {
-  console.log("Fired after 8s");
-  console.log("Scheduling yet another timeouts...");
-  setTimeout(2000, () => console.log("Fired after another 2s"));
+hoistedFunction();
+
+function hoistedFunction() {
+  console.log("This function was hoisted!");
 }
