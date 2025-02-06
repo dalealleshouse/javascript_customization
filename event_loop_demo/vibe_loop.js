b2051a503372a32e87c5ee1d0267b68bdcd98d1c
Dale Alleshouse <dale.alleshouse.1@us.af.mil>
2025-02-06 09:46:31 -0500

    WIP

--- event_loop_demo/vibe_loop.js
+++ event_loop_demo/vibe_loop.js
@@ -2,10 +2,5 @@ console.log("Scheduling 2 timeouts...");
 
 setTimeout(1000, () => console.log("Fired after 1s"));
 setTimeout(2000, () => console.log("Fired after 2s"));
-// setTimeout(3000, () => {
-//   console.log("Fired after 3s");
-//   console.log("Scheduling yet another timeout...");
-//   setTimeout(2000, () => console.log("Fired after another 2s"));
-// });
 
 console.log("All timers scheduled!");
